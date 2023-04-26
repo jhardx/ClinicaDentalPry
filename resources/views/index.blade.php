5c8db8a71ef817524081b9fca87a1f7aa2d7a623
Sebastian Fernandez Seminario <sebasfer2504@outlook.com>
2023-04-26 11:56:16 -0500

    Primer commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,112 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
     <div class="container-fluid">
-        <nav class="navbar fixed-top navbar-expand-lg nav-olla">
+        <img src="{{asset('images/inicio-img.jpg')}}" alt="" class="inicio-img">
+
+        <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">
-                <img src="" alt="Logo" class="d-inline-block align-text-top">
-              </a>
-              <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse nav-flex" id="navbarNav">
-                    <ul class="navbar-nav ">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#scrollspyHeading1">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#scrollspyHeading1">Servicios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#scrollspyHeading1">Expertos</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#scrollspyHeading1">Nosotros</a>
-                    </li>
+                <div class="navbar-collapse nav-flex" id="navbarNav">
+                    <ul class="navbar-nav nav-flex">
+                        <li class="nav-item">
+                            <span><img src="{{asset('images/logo-clinica.jpg')}}" alt="" class="logo-inicio"></span>
+                        </li>
+                        <li class="nav-item">
+                            <span>123123213123</span>
+                        </li>
+                        <li class="nav-item">
+                            <span>Av.Las flores</span>
+                        </li>
+                        <li class="nav-item">
+                            <span>Lun/Vier - 8:00am a 10pm </span>
+                        </li>
                     </ul>
-              </div>
-              <div class="collapse navbar-collapse nav-flex" id="navbarNav">
-                <ul class="navbar-nav ">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#scrollspyHeading1">Iniciar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#scrollspyHeading1">Registrar</a>
-                    </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-
-        <div class="banner">
-            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="https://enfoquedental.com/wp-content/uploads/2021/10/montar-una-clinica-dental.jpg" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="https://enfoquedental.com/wp-content/uploads/2021/10/montar-una-clinica-dental.jpg" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="https://enfoquedental.com/wp-content/uploads/2021/10/montar-una-clinica-dental.jpg" class="d-block w-100" alt="...">
-                  </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
+            </div>
+        </nav>
+        <nav class="navbar sticky-top navbar-expand-lg">
+            <div class="container-fluid">
+                <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-        </div>
-
-        <footer class="row">
-            <div class="telefono col-6">
-                <div><img src="" alt=""></div>
-                <div>
-                    <div>1-800-1254-567</div>
-                    <div>1-800-5214-654</div>
+                <div class="collapse navbar-collapse nav-flex" id="navbarNav">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#scrollspyHeading1">INICIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#scrollspyHeading2">SERVICIOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#scrollspyHeading3">NOSOTROS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#scrollspyHeading4">CONTACTANO</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <div class="ubicacion col-6">
-                <div><img src="" alt=""></div>
-                <div>
-                    <div>2150 Fulton Street San Diego</div>
-                    <div>CA 94117-1080 USA</div>
-                </div>
-            </div>
-        </footer>
-
-        <!-- Footer-->
-        {{-- <footer class="footer py-4">
-            
-            <div class="container">
-                <div class="row align-items-center">
-       
-                    <div class="col-lg-4 my-3 my-lg-0">
-                        <div>1-800-1254-567</div>
-                        <div>1-800-5214-654</div>
-                    </div>
-                    <div class="col-lg-4 text-lg-end">
-                        <div>2150 Fulton Street San Diego</div>
-                    <div>CA 94117-1080 USA</div>
-                    </div>
-                </div>
-            </div>
-        </footer> --}}
+        </nav>
+    </div>
+    
+    <div class="">
     </div>
 </body>
+
 </html>
