5f85c0007b6faa2a282f226d028c2d5eb8a9c154
Joel Gerson Cori Chavez <joelcori111@gmail.com>
2023-05-22 14:02:37 -0500

    funciones agragadas al 80%

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cita;
 use Illuminate\Http\Request;
 
 class PacienteController extends Controller
@@ -81,5 +82,67 @@ class PacienteController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    // Buscar cita
+    public function buscarCita(Request $req)
+    {
+        $searchTerm = trim($req->input('search'));
+        
+        $cita = Cita::where('estado_cita', 'LIKE', "%$searchTerm%")
+                    ->orWhere('fecha', 'LIKE', "%$searchTerm%")
+                    ->get();
+
+        return view('search.index', compact('cita'));
+    }
+
+    // Agendar cita Paciente
+
+    public function agendarCitaPaciente(Request $req){
+        $cita = Cita::create([
+            'estado_cita' => $req->input('estado_cita'),
+            'descripcion' => $req->input('descripcion'),
+            'fecha' => $req->input('fecha'),
+            'hora' => $req->input('hora'),
+            'paciente_id' => $req->input('paciente_id'),
+            'agenda_id' => $req->input('agenda_id'),
+            'tratamiento_id' => $req->input('tratamiento_id'),
+        ]);
+    }
+
+    // Modificar cita paciente
+
+    public function modificarCita(Request $req, $id){
+        // Obtener los valores actualizados de la solicitud
+        $datosActualizados = $req->all();
+
+        // Obtener el registro a editar
+        $cita = Cita::find($id);
+
+        // Verificar si el registro existe
+        if (!$cita) {
+        // Registro no encontrado
+        // Realizar alguna acción, mostrar un mensaje de error, etc.
+        }
+
+        // Actualizar los atributos del modelo
+        $cita->fill($datosActualizados);
+
+        // Guardar los cambios
+        $cita->save();
+    }
+
+    // Eliminar cita para paciente
+
+    public function eliminarCita($id){
+        $cita = Cita::find($id);
+
+        if (!$cita) {
+            return redirect()->back()->with('error', 'Registro no encontrado');
+        }
+    
+        $cita->delete();
+    
+        return redirect()->back()->with('success', 'Registro eliminado exitosamente');
     }
 }
